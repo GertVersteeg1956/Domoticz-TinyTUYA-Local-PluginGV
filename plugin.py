2609688c1639702f84ccb7a8c02f4c347ed62241
Gert Versteeg <114498161+GertVersteeg1956@users.noreply.github.com>
2024-09-01 10:46:32 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -365,7 +365,7 @@ def onHandleThread(startup):
                     # Domoticz.Debug(tuya.status())
                     tuya = tinytuya.Device(dev_id=str(dev['id']), address=str(dev['ip']), local_key=str(dev['key']), version=str(dev['version']), connection_timeout=1, connection_retry_limit=1)
                     tuya.detect_available_dps()
-                    Domoticz.Log(str(getConfigItem(dev['id'], 'last_update')))
+                    Domoticz.Log('Device : ' + str(dev['id'])))
                     if float(time.time()) > float(getConfigItem(dev['id'], 'last_update')):
                         tuyastatus = tuya.status()
 #                        Domoticz.Log (str(tuyastatus))
